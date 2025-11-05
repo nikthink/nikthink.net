ea3f535a4a17bb0087ce5f92ffc1e54f2d3da02d
nick <nicolas.m.lacombe@gmail.com>
2025-11-04 23:57:52 -0500

    moved legal-statements page to static directory moved legal-statements page to static directory

--- serve.sh
+++ serve.sh
@@ -5,7 +5,7 @@ hugoBuildFlags="--enableGitInfo --panicOnWarning --printI18nWarnings --printPath
 build() {
     rm -rf ./public/ &&
         hugo -b / $hugoBuildFlags &&
-        cp -a ./legal-statements.html ./changelog.rss ./data/ ./public/ &&
+        cp -a ./changelog.rss ./data/ ./public/ &&
         mkdir ./public/archives/ &&
         tar -zcf ./public/archives/nikthink-net-full-latest.tgz .editorconfig .github/workflows/* ./*
 }
