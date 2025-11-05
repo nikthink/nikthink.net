f72746bcc04f6b63ee0c70ee8ef7c6559b108ec4
nick <nicolas.m.lacombe@gmail.com>
2025-11-05 00:00:21 -0500

    added code to encrypt content (before git commit) and decrypt it (in browser) added code to encrypt content (before git commit) and decrypt it (in browser)

---
+++ enc.mjs
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import process from 'node:process';
+import os from 'node:os';
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
+async function main() {
+    const args = process.argv.slice(2);
+
+    if (args.length !== 1) {
+        printUsage();
+        process.exitCode = 1;
+        return;
+    }
+
+    const textToEncrypt = args[0];
+    const keyBase64 = fs.readFileSync(os.homedir() + '/.crypt-secret-1-nikthinkNet', 'utf-8');
+    
+    const encJson = await encrypt(keyBase64, textToEncrypt);
+    const encJsonBase64 = await (new TextEncoder().encode(encJson).toBase64({alphabet: 'base64url'}));
+
+    process.stdout.write(`/crypt.html?encJsonBase64Url=${encJsonBase64}&cryptEnd\n`);
+}
+
+function printUsage() {
+    process.stderr.write(`usage:\n`);
+    process.stderr.write(`\tenc.mjs TEXT_TO_ENCRYPT\n`);
+}
+
+async function encrypt(keyBase64, message) {
+    const version = 1;
+    const encAlgo = 'AES-GCM';
+    const cryptoApi = crypto;
+    
+    const binKey = Uint8Array.fromBase64(keyBase64);
+    const keyCrypto = await cryptoApi.subtle.importKey("raw", binKey, encAlgo, true, ["encrypt", "decrypt",]);
+    
+    const ivBin = cryptoApi.getRandomValues(new Uint8Array(12));
+    const encBin = await cryptoApi.subtle.encrypt(
+        {name: encAlgo, iv: ivBin},
+        keyCrypto,
+        new TextEncoder().encode(message)
+    );
+    const encBase64 = await (new Uint8Array(encBin).toBase64());
+    const ivBase64 = await (new Uint8Array(ivBin).toBase64());
+
+    return JSON.stringify({v: version, iv: ivBase64, enc: encBase64});
+}
+
+main().then();
--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,158 @@ async function main() {
     const tablesToSort = document.querySelectorAll('table[data-sort-table]');
 
     tablesToSort.forEach(table => new Tablesort(table));
+
+    if (
+        typeof location === 'object' &&
+        typeof location.href === 'string' &&
+        /\/crypt(\.html)(\?.*)?$/.test(location.href)
+    ) {
+        try {
+            await cryptInit();
+        } catch(error) {
+            console.error(`error during cryptInit(): ${error}`, error);
+        }
+    }
+
+    try {
+        await cryptMountOnLinks();
+    } catch(error) {
+        console.error(`error during cryptMountOnLinks(): ${error}`, error);
+    }
 }
 
 window.onload = function() {
     main().then();
 };
+
+async function cryptInit() {
+    const queryParams = new URLSearchParams(window.location.search);
+    const encJsonBase64Url = queryParams.get('encJsonBase64Url');
+    const drawerSummaryElement = document.querySelector('#show-drawer');
+    const decryptedMessageElement = document.querySelector('#decrypted-message');
+
+    if (!encJsonBase64Url)
+        return;
+    
+    const encJson = new TextDecoder().decode(Uint8Array.fromBase64(encJsonBase64Url));
+
+    const keyBase64 = await cryptGetKeyBase64('keyBase64');
+
+    if (!keyBase64)
+        throw new Error(`error getting key: ${keyBase64}`);
+
+    const dec = await (new SimpleCrypto(keyBase64).decrypt(encJson));
+
+    decryptedMessageElement.innerText = dec;
+    drawerSummaryElement.innerText = 'Show';
+    window.history.replaceState({}, document.title, location.href.split('?')[0]);
+}
+
+async function cryptMountOnLinks() {
+    const linkRegex = /\/crypt\.html\?encJsonBase64Url=([A-Za-z0-9\+\/=]+)&cryptEnd/;
+    const linkElements = document.querySelectorAll('a');
+
+    if (!linkElements)
+        return;
+
+    linkElements.forEach(linkElement => {
+        let match;
+        
+        if (match = linkRegex.exec(linkElement.href)) {
+            const origHref = linkElement.href;
+            const encJsonBase64Url = match[1];
+
+            console.info(`detected crypt link`, {encJsonBase64Url: encJsonBase64Url});
+            
+            linkElement.addEventListener('click', async () => {
+                console.info('user clicked on encrypted link. trying to decrypt all links.');
+
+                try {
+                    await cryptDecryptAllLinks();
+                } catch(error) {
+                    console.error(`error during cryptDecryptAllLinks(): ${error}`, error);
+                }
+            });
+
+            linkElement.dataset.encJsonBase64Url = encJsonBase64Url;
+            linkElement.href = '#';
+        }
+    });
+}
+
+async function cryptDecryptAllLinks() {
+    const linkElements = document.querySelectorAll('a');
+
+    if (!linkElements)
+        return;
+    
+    linkElements.forEach(async (linkElement) => {
+        try {
+            const encJsonBase64Url = linkElement.dataset.encJsonBase64Url;
+
+            if (!encJsonBase64Url)
+                return;
+            
+            const encJson = new TextDecoder().decode(Uint8Array.fromBase64(encJsonBase64Url));
+            const keyBase64 = await cryptGetKeyBase64('keyBase64');
+            
+            if (!keyBase64)
+                throw new Error(`error getting key: ${keyBase64}`);
+            
+            const dec = await (new SimpleCrypto(keyBase64).decrypt(encJson));
+    
+            linkElement.innerText = dec;
+        } catch(error) {
+            console.error(`error decrypting link: ${error}`, error);
+        }
+    });
+}
+
+async function cryptGetKeyBase64(keyName) {
+    const lsKey = localStorage.getItem(keyName);
+
+    if (!!lsKey && lsKey !== 'null' && lsKey !== 'undefined' && lsKey !== '')
+        return lsKey;
+
+    localStorage.removeItem(keyName);
+
+    const key = window.prompt(`${keyName}: `);
+
+    if (!!key)
+        localStorage.setItem(keyName, key);
+
+    return key;
+}
+
+class SimpleCrypto {
+    #version = 1;
+    #encAlgo = 'AES-GCM';
+    #cryptoApi
+    #key;
+
+    constructor(keyBase64) {
+        this.#key = keyBase64;
+    }
+
+    async decrypt(encJson) {
+        const cryptoApi = window.crypto;
+        
+        const binKey = Uint8Array.fromBase64(this.#key);
+        const keyCrypto = await cryptoApi.subtle.importKey("raw", binKey, this.#encAlgo, true, ["encrypt", "decrypt"]);
+
+        const encObj = JSON.parse(encJson);
+
+        if (encObj.v !== this.#version)
+            throw new Error(`error: wrong version. this class is v${this.#version}, and the object: ${encObj.v}`);
+
+        const ivBin = Uint8Array.fromBase64(encObj.iv);
+        const encBin = Uint8Array.fromBase64(encObj.enc);
+        const decBin = await cryptoApi.subtle.decrypt(
+            {name: this.#encAlgo, iv: ivBin},
+            keyCrypto,
+            encBin
+        );
+        
+        return new TextDecoder().decode(decBin);
+    }
+}
